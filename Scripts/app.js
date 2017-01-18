59ea63a090a2228ff40995e8f239c943a26e040a
Joshua Dusome <failnix@hotmail.com>
2017-01-18 14:49:42 -0500

    added some basic javascript with an event listener and handler

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,3 +1,14 @@
 /* JavaScript lives here */
 
 console.log("App Started");
+
+//Assigns the button in our index to a variable
+var clickMeButton = document.getElementById("clickMeButton");
+
+//adds an event listener to trigger when the clickMeButton is clicked. It will call the Click function.
+clickMeButton.addEventListener("click", Click);
+
+// create a Click function, which will log to the console when we click. Used as an event handler.
+function Click() {
+    console.log("Clicked...");
+}
